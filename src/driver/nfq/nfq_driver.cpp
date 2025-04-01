39f357574cd7b8928a313d32e55990c281e3aad9
Voevodin Kirill <voev.kirill@gmail.com>
2025-04-02 01:34:14 +0300

    fixes

--- src/driver/nfq/nfq_driver.cpp
+++ src/driver/nfq/nfq_driver.cpp
@@ -6,10 +6,9 @@
 
 #include <system_error>
 
+#include <linux/netlink.h>
 #include <linux/netfilter.h>
 #include <linux/netfilter/nfnetlink_queue.h>
-
-#include <libmnl/libmnl.h>
 
 #include <snet/io/packet_pool.hpp>
 #include <snet/socket.hpp>
@@ -29,41 +28,64 @@ namespace snet::driver
 {
 
 #define NFQ_DEFAULT_POOL_SIZE 16
-#define DEFAULT_QUEUE_MAXLEN 1024 // Based on NFQNL_QMAX_DEFAULT from nfnetlnk_queue_core.c
+#define DEFAULT_QUEUE_MAXLEN 1024
 
 constexpr size_t align_length(size_t len, size_t alignment = 4) noexcept
 {
     return (len + alignment - 1) & ~(alignment - 1);
 }
 
-void* get_payload(const nlmsghdr* nlh)
+bool MessageIsOk(const nlmsghdr* nlh, int len)
+{
+    return len >= (int)sizeof(nlmsghdr) && nlh->nlmsg_len >= sizeof(nlmsghdr) &&
+           (int)nlh->nlmsg_len <= len;
+}
+
+nlmsghdr* MessageNext(const nlmsghdr* nlh, int* len)
+{
+    *len -= align_length(nlh->nlmsg_len);
+    return (nlmsghdr*)((uint8_t*)nlh + align_length(nlh->nlmsg_len));
+}
+
+void* MessageGetPayload(const nlmsghdr* nlh)
 {
     return (uint8_t*)nlh + align_length(sizeof(nlmsghdr));
 }
 
-void* get_payload_offset(const nlmsghdr* nlh, size_t offset)
+void* MessageGetPayloadOffset(const nlmsghdr* nlh, size_t offset)
 {
     return (uint8_t*)nlh + align_length(sizeof(nlmsghdr)) + align_length(offset);
 }
 
-void* get_payload_tail(const nlmsghdr* nlh)
+void* MessageGetPayloadTail(const nlmsghdr* nlh)
 {
     return (uint8_t*)nlh + align_length(nlh->nlmsg_len);
 }
 
-uint16_t attr_get_payload_len(const struct nlattr* attr)
+uint16_t AttrGetPayloadLen(const nlattr* attr)
 {
     return attr->nla_len - align_length(sizeof(nlattr));
 }
 
-void* attr_get_payload(const struct nlattr* attr)
+void* AttrGetPayload(const nlattr* attr)
 {
     return (uint8_t*)attr + align_length(sizeof(nlattr));
 }
 
-uint32_t attr_get_u32(const struct nlattr* attr)
+uint32_t AttrGetUint32(const nlattr* attr)
 {
-    return *((uint32_t*)attr_get_payload(attr));
+    return *((uint32_t*)AttrGetPayload(attr));
+}
+
+bool AttrIsOk(const nlattr* attr, int len)
+{
+    return len >= (int)sizeof(nlattr) && attr->nla_len >= sizeof(nlattr) &&
+           (int)attr->nla_len <= len;
+}
+
+nlattr* AttrNext(const nlattr* attr)
+{
+    return (nlattr*)((uint8_t*)attr + align_length(attr->nla_len));
 }
 
 static inline nlmsghdr* CreateNetfilterHeader(void* buf, int type, uint32_t queue_num)
@@ -89,9 +111,9 @@ static inline nlmsghdr* CreateNetfilterHeader(void* buf, int type, uint32_t queu
     return nlh;
 }
 
-void attr_put(struct nlmsghdr* nlh, uint16_t type, size_t len, const void* data)
+void SetAttribute(nlmsghdr* nlh, uint16_t type, size_t len, const void* data)
 {
-    nlattr* attr = (nlattr*)get_payload_tail(nlh);
+    nlattr* attr = (nlattr*)MessageGetPayloadTail(nlh);
     uint16_t payload_len = align_length(sizeof(nlattr)) + len;
     int pad;
 
@@ -99,58 +121,56 @@ void attr_put(struct nlmsghdr* nlh, uint16_t type, size_t len, const void* data)
     attr->nla_len = payload_len;
 
     memcpy((uint8_t*)attr + align_length(sizeof(nlattr)), data, len);
-    pad = MNL_ALIGN(len) - len;
+    pad = align_length(len) - len;
     if (pad > 0)
         memset((uint8_t*)attr + align_length(sizeof(nlattr)) + len, 0, pad);
 
-    nlh->nlmsg_len += MNL_ALIGN(payload_len);
+    nlh->nlmsg_len += align_length(payload_len);
 }
 
-static nlmsghdr* nfq_build_cfg_command(char* buf, uint16_t pf, uint8_t command, int queue_num)
+static nlmsghdr* SetCfgCommand(char* buf, uint16_t pf, uint8_t command, int queue_num)
 {
     nlmsghdr* nlh = CreateNetfilterHeader(buf, NFQNL_MSG_CONFIG, queue_num);
     nfqnl_msg_config_cmd cmd;
     cmd.command = command;
     cmd.pf = htons(pf);
 
-    attr_put(nlh, NFQA_CFG_CMD, sizeof(cmd), &cmd);
+    SetAttribute(nlh, NFQA_CFG_CMD, sizeof(cmd), &cmd);
     return nlh;
 }
 
-static nlmsghdr* nfq_build_cfg_params(char* buf, uint8_t mode, int range, int queue_num)
+static nlmsghdr* SetCfgParams(char* buf, uint8_t mode, int range, int queue_num)
 {
     nlmsghdr* nlh = CreateNetfilterHeader(buf, NFQNL_MSG_CONFIG, queue_num);
     nfqnl_msg_config_params params;
     params.copy_range = htonl(range);
     params.copy_mode = mode;
 
-    attr_put(nlh, NFQA_CFG_PARAMS, sizeof(params), &params);
+    SetAttribute(nlh, NFQA_CFG_PARAMS, sizeof(params), &params);
     return nlh;
 }
 
-static nlmsghdr* nfq_build_verdict(char* buf, int id, int queue_num, int verd, uint32_t plen,
-                                   uint8_t* pkt)
+static nlmsghdr* SetVerdict(char* buf, int id, int queue_num, int verd, uint32_t plen, uint8_t* pkt)
 {
     nlmsghdr* nlh = CreateNetfilterHeader(buf, NFQNL_MSG_VERDICT, queue_num);
     nfqnl_msg_verdict_hdr verdictHeader;
     verdictHeader.verdict = htonl(verd);
     verdictHeader.id = htonl(id);
 
-    attr_put(nlh, NFQA_VERDICT_HDR, sizeof(verdictHeader), &verdictHeader);
+    SetAttribute(nlh, NFQA_VERDICT_HDR, sizeof(verdictHeader), &verdictHeader);
     if (plen)
-        attr_put(nlh, NFQA_PAYLOAD, plen, pkt);
+        SetAttribute(nlh, NFQA_PAYLOAD, plen, pkt);
 
     return nlh;
 }
 
-static int parse_attr_cb(const nlattr* attr, void* data)
+static int ParseAttr(const nlattr* attr, void* data)
 {
     const nlattr** tb = (const nlattr**)data;
     int type = attr->nla_type & NLA_TYPE_MASK;
 
-    /* skip unsupported attribute in user-space */
     if (type > NFQA_MAX)
-        return MNL_CB_OK;
+        return 1;
 
     switch (type)
     {
@@ -165,23 +185,25 @@ static int parse_attr_cb(const nlattr* attr, void* data)
     case NFQA_UID:
     case NFQA_GID:
     case NFQA_CT_INFO:
-        if (mnl_attr_validate(attr, MNL_TYPE_U32) < 0)
+        if (AttrGetPayloadLen(attr) != sizeof(uint32_t))
+        {
             return -1;
+        }
         break;
     case NFQA_TIMESTAMP:
-        if (mnl_attr_validate2(attr, MNL_TYPE_UNSPEC, sizeof(nfqnl_msg_packet_timestamp)) < 0)
+        if (AttrGetPayloadLen(attr) != sizeof(nfqnl_msg_packet_timestamp))
         {
             return -1;
         }
         break;
     case NFQA_HWADDR:
-        if (mnl_attr_validate2(attr, MNL_TYPE_UNSPEC, sizeof(nfqnl_msg_packet_hw)) < 0)
+        if (AttrGetPayloadLen(attr) != sizeof(nfqnl_msg_packet_hw))
         {
             return -1;
         }
         break;
     case NFQA_PACKET_HDR:
-        if (mnl_attr_validate2(attr, MNL_TYPE_UNSPEC, sizeof(nfqnl_msg_packet_hdr)) < 0)
+        if (AttrGetPayloadLen(attr) != sizeof(nfqnl_msg_packet_hdr))
         {
             return -1;
         }
@@ -192,41 +214,18 @@ static int parse_attr_cb(const nlattr* attr, void* data)
         break;
     }
     tb[type] = attr;
-    return MNL_CB_OK;
+    return 1;
 }
 
-bool nlmsg_ok(const nlmsghdr* nlh, int len)
+int ParseAttrs(const nlmsghdr* nlh, unsigned int offset, void* data)
 {
-    return len >= (int)sizeof(nlmsghdr) && nlh->nlmsg_len >= sizeof(nlmsghdr) &&
-           (int)nlh->nlmsg_len <= len;
-}
+    int ret = 1;
+    const nlattr* attr;
 
-nlmsghdr* nlmsg_next(const nlmsghdr* nlh, int* len)
-{
-    *len -= align_length(nlh->nlmsg_len);
-    return (nlmsghdr*)((uint8_t*)nlh + align_length(nlh->nlmsg_len));
-}
-
-bool attr_ok(const nlattr* attr, int len)
-{
-    return len >= (int)sizeof(struct nlattr) && attr->nla_len >= sizeof(struct nlattr) &&
-           (int)attr->nla_len <= len;
-}
-
-struct nlattr* attr_next(const nlattr* attr)
-{
-    return (nlattr*)((uint8_t*)attr + align_length(attr->nla_len));
-}
-
-int attr_parse(const nlmsghdr* nlh, unsigned int offset, void* data)
-{
-    int ret = MNL_CB_OK;
-    const struct nlattr* attr;
-
-    for (attr = (nlattr*)get_payload_offset(nlh, offset);
-         attr_ok(attr, (char*)get_payload_tail(nlh) - (char*)(attr)); attr = attr_next(attr))
+    for (attr = (nlattr*)MessageGetPayloadOffset(nlh, offset);
+         AttrIsOk(attr, (char*)MessageGetPayloadTail(nlh) - (char*)(attr)); attr = AttrNext(attr))
     {
-        if ((ret = parse_attr_cb(attr, data)) <= 0)
+        if ((ret = ParseAttr(attr, data)) <= 0)
             return ret;
     }
     return ret;
@@ -238,50 +237,45 @@ static int process_message_cb(const nlmsghdr* nlh, void* data)
     nlattr* attr[NFQA_MAX + 1] = {};
     int ret;
 
-    /* FIXIT-L In the event that there is actually more than one packet per message, handle it
-       gracefully. I haven't actually seen this happen yet. */
     if (rawPacket->mh)
         return -1;
 
-    /* Parse the message attributes */
-    if ((ret = attr_parse(nlh, sizeof(nfgenmsg), attr)) != MNL_CB_OK)
+    if ((ret = ParseAttrs(nlh, sizeof(nfgenmsg), attr)) != 1)
         return ret;
 
-    /* Populate the packet descriptor */
     rawPacket->mh = nlh;
-    rawPacket->ph = (nfqnl_msg_packet_hdr*)attr_get_payload(attr[NFQA_PACKET_HDR]);
+    rawPacket->ph = (nfqnl_msg_packet_hdr*)AttrGetPayload(attr[NFQA_PACKET_HDR]);
 
     timeval tv{};
     gettimeofday(&tv, NULL);
 
     size_t pktlen;
-    size_t framelen = attr_get_payload_len(attr[NFQA_PAYLOAD]);
+    size_t framelen = AttrGetPayloadLen(attr[NFQA_PAYLOAD]);
     if (attr[NFQA_CAP_LEN])
-        pktlen = ntohl(attr_get_u32(attr[NFQA_CAP_LEN]));
+        pktlen = ntohl(AttrGetUint32(attr[NFQA_CAP_LEN]));
     else
         pktlen = framelen;
 
-    rawPacket->setRawData((uint8_t*)attr_get_payload(attr[NFQA_PAYLOAD]), pktlen, tv,
+    rawPacket->setRawData((uint8_t*)AttrGetPayload(attr[NFQA_PAYLOAD]), pktlen, tv,
                           io::LINKTYPE_RAW, framelen);
 
-    return MNL_CB_OK;
+    return 1;
 }
 
 int cb_run_my(const void* buf, size_t numbytes, unsigned int seq, unsigned int portid, void* data)
 {
-    int ret = MNL_CB_OK, len = numbytes;
-    const struct nlmsghdr* nlh = (struct nlmsghdr*)buf;
+    int ret = 1, len = numbytes;
+    const nlmsghdr* nlh = (nlmsghdr*)buf;
 
-    while (nlmsg_ok(nlh, len))
+    while (MessageIsOk(nlh, len))
     {
-        /* check message source */
         auto v = nlh->nlmsg_pid && portid ? nlh->nlmsg_pid == portid : true;
         if (!v)
         {
             errno = ESRCH;
             return -1;
         }
-        /* perform sequence tracking */
+
         v = nlh->nlmsg_seq && seq ? nlh->nlmsg_seq == seq : true;
         if (!v)
         {
@@ -289,14 +283,12 @@ int cb_run_my(const void* buf, size_t numbytes, unsigned int seq, unsigned int p
             return -1;
         }
 
-        /* dump was interrupted */
         if (nlh->nlmsg_flags & NLM_F_DUMP_INTR)
         {
             errno = EINTR;
             return -1;
         }
 
-        /* netlink data message handling */
         if (nlh->nlmsg_type >= NLMSG_MIN_TYPE)
         {
             ret = process_message_cb(nlh, data);
@@ -305,14 +297,14 @@ int cb_run_my(const void* buf, size_t numbytes, unsigned int seq, unsigned int p
         }
         else if (nlh->nlmsg_type == NLMSG_ERROR)
         {
-            const struct nlmsgerr* err = (struct nlmsgerr*)get_payload(nlh);
+            const nlmsgerr* err = (nlmsgerr*)MessageGetPayload(nlh);
 
             if (nlh->nlmsg_len < sizeof(nlmsgerr) + align_length(sizeof(nlmsghdr)))
             {
                 errno = EBADMSG;
                 return -1;
             }
-            /* Netlink subsystems returns the errno value with different signess */
+
             if (err->error < 0)
                 errno = -err->error;
             else
@@ -325,7 +317,7 @@ int cb_run_my(const void* buf, size_t numbytes, unsigned int seq, unsigned int p
             ret = 0;
             goto out;
         }
-        nlh = nlmsg_next(nlh, &len);
+        nlh = MessageNext(nlh, &len);
     }
 out:
     return ret;
@@ -336,14 +328,21 @@ struct NfQueue::Impl
     Impl();
     ~Impl() noexcept;
 
-    ssize_t socket_sendto(const void* buf, size_t len)
+    ssize_t sendSocket(const void* buf, size_t len, std::error_code& ec) noexcept
     {
         sockaddr_nl snl{};
         snl.nl_family = AF_NETLINK;
-        return ::sendto(socket, buf, len, 0, (sockaddr*)&snl, sizeof(snl));
+        ssize_t ret;
+
+        ret = ::sendto(socket, buf, len, 0, (sockaddr*)&snl, sizeof(snl));
+        if (ret == -1)
+        {
+            ec = GetLastSystemError();
+        }
+        return ret;
     }
 
-    ssize_t socket_recv(void* buf, size_t bufsiz, bool blocking, std::error_code& ec)
+    ssize_t recvSocket(void* buf, size_t bufsiz, bool blocking, std::error_code& ec) noexcept
     {
         ec.clear();
 
@@ -473,16 +472,12 @@ NfQueue::NfQueue(const io::DriverConfig& config)
             casket::utils::to_number(value, impl_->queue_maxlen, ec);
     }
 
-    impl_->nlmsg_bufsize = impl_->snaplen + MNL_SOCKET_BUFFER_SIZE;
-
-    /* Allocate a scratch buffer for general usage by the context (basically for anything that's not
-        receiving a packet) */
+    impl_->nlmsg_bufsize = impl_->snaplen + 4096;
     impl_->nlmsg_buf = (char*)malloc(impl_->nlmsg_bufsize);
 
     impl_->pool.allocatePool(NFQ_DEFAULT_POOL_SIZE);
     impl_->socket = socket::CreateSocket(AF_NETLINK, SOCK_RAW, NETLINK_NETFILTER, ec);
 
-    /* Implement the requested timeout by way of the receive timeout on the netlink socket */
     impl_->timeout = base.getTimeout();
     if (impl_->timeout)
     {
@@ -492,11 +487,6 @@ NfQueue::NfQueue(const io::DriverConfig& config)
         setsockopt(impl_->socket, SOL_SOCKET, SO_RCVTIMEO, (const void*)&tv, sizeof(tv));
     }
 
-    /* Set the socket receive buffer to something reasonable based on the desired queue and capture
-       lengths. Try with FORCE first to allow overriding the system's global rmem_max, then fall
-       back on being limited by it if that doesn't work. The value will be doubled to allow room for
-       bookkeeping overhead, so the default of 1024 * 1500 will end up allocating about 3MB of
-       receive buffer space.  The unmodified default tends to be around 208KB. */
     unsigned int socket_rcvbuf_size = impl_->queue_maxlen * impl_->snaplen;
     if (setsockopt(impl_->socket, SOL_SOCKET, SO_RCVBUFFORCE, &socket_rcvbuf_size,
                    sizeof(socket_rcvbuf_size)) == -1)
@@ -509,49 +499,38 @@ NfQueue::NfQueue(const io::DriverConfig& config)
 
     nlmsghdr* nlh;
 
-    /* The following four packet family unbind/bind commands do nothing on modern (3.8+) kernels.
-        They used to handle binding the netfilter socket to a particular address family. */
-    nlh = nfq_build_cfg_command(impl_->nlmsg_buf, AF_INET, NFQNL_CFG_CMD_PF_UNBIND, 0);
-    impl_->socket_sendto(nlh, nlh->nlmsg_len);
+    nlh = SetCfgCommand(impl_->nlmsg_buf, AF_INET, NFQNL_CFG_CMD_PF_UNBIND, 0);
+    impl_->sendSocket(nlh, nlh->nlmsg_len, ec);
 
-    nlh = nfq_build_cfg_command(impl_->nlmsg_buf, AF_INET6, NFQNL_CFG_CMD_PF_UNBIND, 0);
-    impl_->socket_sendto(nlh, nlh->nlmsg_len);
+    nlh = SetCfgCommand(impl_->nlmsg_buf, AF_INET6, NFQNL_CFG_CMD_PF_UNBIND, 0);
+    impl_->sendSocket(nlh, nlh->nlmsg_len, ec);
 
-    nlh = nfq_build_cfg_command(impl_->nlmsg_buf, AF_INET, NFQNL_CFG_CMD_PF_BIND, 0);
-    impl_->socket_sendto(nlh, nlh->nlmsg_len);
+    nlh = SetCfgCommand(impl_->nlmsg_buf, AF_INET, NFQNL_CFG_CMD_PF_BIND, 0);
+    impl_->sendSocket(nlh, nlh->nlmsg_len, ec);
 
-    nlh = nfq_build_cfg_command(impl_->nlmsg_buf, AF_INET6, NFQNL_CFG_CMD_PF_BIND, 0);
-    impl_->socket_sendto(nlh, nlh->nlmsg_len);
+    nlh = SetCfgCommand(impl_->nlmsg_buf, AF_INET6, NFQNL_CFG_CMD_PF_BIND, 0);
+    impl_->sendSocket(nlh, nlh->nlmsg_len, ec);
 
-    /* Now, actually bind to the netfilter queue.  The address family specified is irrelevant. */
-    nlh = nfq_build_cfg_command(impl_->nlmsg_buf, AF_UNSPEC, NFQNL_CFG_CMD_BIND, impl_->queue_num);
-    impl_->socket_sendto(nlh, nlh->nlmsg_len);
+    nlh = SetCfgCommand(impl_->nlmsg_buf, AF_UNSPEC, NFQNL_CFG_CMD_BIND, impl_->queue_num);
+    impl_->sendSocket(nlh, nlh->nlmsg_len, ec);
 
-    /*
-     * Set the queue into packet copying mode with a max copying length of our snaplen.
-     * While we're building a configuration message, we might as well tack on our requested
-     * maximum queue length and enable delivery of packets that will be subject to GSO. That
-     * last bit means we'll potentially see packets larger than the device MTU prior to their
-     * trip through the segmentation offload path.  They'll probably show up as truncated.
-     */
-    nlh =
-        nfq_build_cfg_params(impl_->nlmsg_buf, NFQNL_COPY_PACKET, impl_->snaplen, impl_->queue_num);
+    nlh = SetCfgParams(impl_->nlmsg_buf, NFQNL_COPY_PACKET, impl_->snaplen, impl_->queue_num);
 
     uint32_t value = htonl(impl_->queue_maxlen);
-    attr_put(nlh, NFQA_CFG_QUEUE_MAXLEN, sizeof(value), &value);
+    SetAttribute(nlh, NFQA_CFG_QUEUE_MAXLEN, sizeof(value), &value);
 
     value = htonl(NFQA_CFG_F_GSO);
-    attr_put(nlh, NFQA_CFG_FLAGS, sizeof(value), &value);
-    attr_put(nlh, NFQA_CFG_MASK, sizeof(value), &value);
+    SetAttribute(nlh, NFQA_CFG_FLAGS, sizeof(value), &value);
+    SetAttribute(nlh, NFQA_CFG_MASK, sizeof(value), &value);
 
     if (impl_->fail_open)
     {
         value = htonl(NFQA_CFG_F_FAIL_OPEN);
-        attr_put(nlh, NFQA_CFG_FLAGS, sizeof(value), &value);
-        attr_put(nlh, NFQA_CFG_MASK, sizeof(value), &value);
+        SetAttribute(nlh, NFQA_CFG_FLAGS, sizeof(value), &value);
+        SetAttribute(nlh, NFQA_CFG_MASK, sizeof(value), &value);
     }
 
-    impl_->socket_sendto(nlh, nlh->nlmsg_len);
+    impl_->sendSocket(nlh, nlh->nlmsg_len, ec);
 }
 
 std::shared_ptr<io::Driver> NfQueue::create(const io::DriverConfig& config)
@@ -577,9 +556,11 @@ Status NfQueue::interrupt()
 
 Status NfQueue::stop()
 {
-    nlmsghdr* nlh =
-        nfq_build_cfg_command(impl_->nlmsg_buf, AF_INET, NFQNL_CFG_CMD_UNBIND, impl_->queue_num);
-    if (impl_->socket_sendto(nlh, nlh->nlmsg_len) == -1)
+    nlmsghdr* nlh;
+    std::error_code ec;
+
+    nlh = SetCfgCommand(impl_->nlmsg_buf, AF_INET, NFQNL_CFG_CMD_UNBIND, impl_->queue_num);
+    if (impl_->sendSocket(nlh, nlh->nlmsg_len, ec) == -1)
     {
         return Status::Error;
     }
@@ -637,7 +618,7 @@ RecvStatus NfQueue::receivePacket(io::RawPacket** pRawPacket)
     ssize_t ret;
     do
     {
-        ret = impl_->socket_recv(rawPacket->nlmsg_buf, impl_->nlmsg_bufsize, true, ec);
+        ret = impl_->recvSocket(rawPacket->nlmsg_buf, impl_->nlmsg_bufsize, true, ec);
         if (ret < 0)
         {
             if (ec == std::errc::no_buffer_space)
@@ -682,11 +663,7 @@ RecvStatus NfQueue::receivePacket(io::RawPacket** pRawPacket)
 
 Status NfQueue::finalizePacket(io::RawPacket* rawPacket, Verdict verdict)
 {
-
-    /* Send the verdict back to the kernel through netlink */
-    /* FIXIT-L Consider using an iovec for scatter/gather transmission with the new payload as a
-        separate entry. This would avoid a copy and potentially avoid buffer size restrictions.
-        Only as relevant as REPLACE is common. */
+    std::error_code ec;
     uint32_t plen = (verdict == Verdict::Verdict_REPLACE) ? rawPacket->getRawDataLen() : 0;
     int nfq_verdict = (verdict == Verdict::Verdict_PASS || verdict == Verdict::Verdict_REPLACE)
                           ? NF_ACCEPT
@@ -694,10 +671,10 @@ Status NfQueue::finalizePacket(io::RawPacket* rawPacket, Verdict verdict)
 
     auto nlPacket = dynamic_cast<NfqRawPacket*>(rawPacket);
 
-    nlmsghdr* nlh =
-        nfq_build_verdict(impl_->nlmsg_buf, ntohl(nlPacket->ph->packet_id), impl_->queue_num,
-                          nfq_verdict, plen, (uint8_t*)rawPacket->getRawData());
-    if (impl_->socket_sendto(nlh, nlh->nlmsg_len) == -1)
+    nlmsghdr* nlh = SetVerdict(impl_->nlmsg_buf, ntohl(nlPacket->ph->packet_id), impl_->queue_num,
+                               nfq_verdict, plen, (uint8_t*)rawPacket->getRawData());
+
+    if (impl_->sendSocket(nlh, nlh->nlmsg_len, ec) == -1)
     {
         return Status::Error;
     }
