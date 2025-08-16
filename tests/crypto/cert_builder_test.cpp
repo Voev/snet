d6fd7b1c7318f3c8b3dafe977f9841e23f66a4b8
Voevodin Kirill <voev.kirill@gmail.com>
2025-08-16 18:08:01 +0300

    delete snet_ip from tls dependencies

--- tests/crypto/cert_builder_test.cpp
+++ tests/crypto/cert_builder_test.cpp
@@ -58,7 +58,7 @@ TEST_F(CertBuilderTest, CreateSelfSignedCert)
     ASSERT_TRUE(CertName::isEqual(Cert::subjectName(cert), name));
     ASSERT_TRUE(CertName::isEqual(Cert::issuerName(cert), name));
     ASSERT_TRUE(AsymmKey::isEqual(key_, Cert::publicKey(cert)));
-    ASSERT_TRUE(X509_self_signed(cert, true));
+    ASSERT_TRUE(Cert::isSelfSigned(cert, true));
 }
 
 TEST_F(CertBuilderTest, CreateChildCert)
@@ -85,7 +85,7 @@ TEST_F(CertBuilderTest, CreateChildCert)
 
     X509CertPtr ca;
     ASSERT_NO_THROW(ca = caBuilder.build());
-    ASSERT_TRUE(X509_self_signed(ca, true));
+    ASSERT_TRUE(Cert::isSelfSigned(ca, true));
 
     auto childKey = RsaAsymmKey::generate(2048);
 
