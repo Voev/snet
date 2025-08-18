4e4eca88672a894459b7fcd25c7383b30267a0fe
Voevodin Kirill <voev.kirill@gmail.com>
2025-08-18 20:15:53 +0300

    derive tests are added

--- tests/crypto/group_params_test.cpp
+++ tests/crypto/group_params_test.cpp
@@ -6,18 +6,18 @@
 using namespace snet::crypto;
 using namespace testing;
 
-class GroupParamsTest : public TestWithParam<GroupParams>
+class GroupParamsGenerationTest : public TestWithParam<GroupParams>
 {
 };
 
-TEST_P(GroupParamsTest, GenerateKeyByParams)
+TEST_P(GroupParamsGenerationTest, GenerateKeyByParams)
 {
     KeyPtr key;
     ASSERT_NO_THROW(key = GroupParams::generateKeyByParams(GetParam()));
     ASSERT_NE(key, nullptr);
 }
 
-TEST_P(GroupParamsTest, GenerateParamsThenKey)
+TEST_P(GroupParamsGenerationTest, GenerateParamsThenKey)
 {
     KeyPtr params;
     ASSERT_NO_THROW(params = GroupParams::generateParams(GetParam()));
@@ -25,4 +25,30 @@ TEST_P(GroupParamsTest, GenerateParamsThenKey)
     ASSERT_NO_THROW(params = GroupParams::generateKeyByParams(params));
 }
 
-INSTANTIATE_TEST_CASE_P(EC, GroupParamsTest, ValuesIn(GroupParams::getSupported()));
+INSTANTIATE_TEST_SUITE_P(EC, GroupParamsGenerationTest, ValuesIn(GroupParams::getSupported()));
+
+
+using DeriveTestParam = std::tuple<GroupParams, bool>;
+class GroupParamsDeriveTest : public TestWithParam<DeriveTestParam>
+{
+};
+
+TEST_P(GroupParamsDeriveTest, DeriveSharedKey)
+{
+    KeyPtr clientKey;
+    KeyPtr serverKey;
+    
+    const auto& params = GetParam();
+
+    ASSERT_NO_THROW(clientKey = GroupParams::generateKeyByParams(std::get<0>(params)));
+    ASSERT_NO_THROW(serverKey = GroupParams::generateKeyByParams(std::get<0>(params)));
+
+    std::vector<uint8_t> sharedKeyA;
+    std::vector<uint8_t> sharedKeyB;
+    ASSERT_NO_THROW(sharedKeyA = GroupParams::deriveSecret(clientKey, serverKey, std::get<1>(params)));
+    ASSERT_NO_THROW(sharedKeyB = GroupParams::deriveSecret(serverKey, clientKey, std::get<1>(params)));
+
+    ASSERT_EQ(sharedKeyA, sharedKeyB);
+}
+
+INSTANTIATE_TEST_SUITE_P(EC, GroupParamsDeriveTest, Combine(ValuesIn(GroupParams::getSupported()), Bool()));
